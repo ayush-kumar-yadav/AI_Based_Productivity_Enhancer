bacd91624f24f8eac420b3034a4f9e4419aa95c2
ayush <ay881009@gmail.com>
2026-03-17 01:05:52 +0530

    extra deleted

--- practice/day3basics/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const taskRoutes = require('./routes/taskRoutes');
-app.use("/tasks",taskRoutes);
-const PORT = 5000;
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`);
-});
--- practice/day3basics/routes/taskRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const{getAllTasks,getTaskById} = require('../controllers/taskControllers');
-router.get("/", getAllTasks);
-router.get("/:id", getTaskById);
-module.exports = router;
